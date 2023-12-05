8b4f4afcbbd150ab1fa314a062530837a18946d1
RiLaFuS <ce26415@gmail.com>
2023-12-05 18:08:20 +0900

    10st commmit

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ if st.sidebar.button("できあがり"):
     st.write(glass_df)
 
     # 予測の実行
-    response = requests.post("/predict", json=glass)
+    response = requests.post("https://glassapp-kh9owc32nt34xnlifhbkgd.streamlit.app/predict", json=glass)
     prediction = response.json()["prediction"]
 
     # 予測結果の表示
