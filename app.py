357529a407d417708395d85789d798ff088876d2
RiLaFuS <ce26415@gmail.com>
2023-12-06 15:59:11 +0900

    10st commmit

--- app.py
+++ app.py
@@ -3,7 +3,53 @@ import pandas as pd
 import requests
 import numpy
 from PIL import Image
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import List
+from sklearn.ensemble import RandomForestClassifier
 
+# インスタンス化
+app = FastAPI()
+
+# 入力するデータ型の定義
+class df(BaseModel):
+    RI: float
+    Na: float
+    Mg: float
+    Al: float
+    Si: float
+    K: float
+    Ca: float
+    Ba: float
+    Fe: float
+
+# モデルの構築
+def build_model():
+    df = pd.read_table('C:\\Users\\ce264\\Desktop\\glass\\train_glass.tsv', index_col=0)
+    features = pd.DataFrame(df[['RI', 'Na', 'Mg', 'Al', 'Si', 'K', 'Ca', 'Ba', 'Fe']], columns=df.columns.tolist()[:-1])
+    target = df['Type']
+
+    model = RandomForestClassifier()
+    model.fit(features, target)
+
+    return model
+
+# 学習済みのモデルの読み込み
+model = build_model()
+
+# /predict エンドポイントの処理
+@app.get('/')
+def index():
+    return {"Glass": 'df_prediction'}
+
+# POST が送信された時（入力）と予測値（出力）の定義
+@app.post('/predict')
+def make_predictions(features: df):
+    return {'prediction': str(model.predict([[features.RI, features.Na, features.Mg, features.Al, features.Si, features.K, features.Ca, features.Ba, features.Fe]])[0])}
+
+# Streamlit アプリケーション
+if st.button("Run Streamlit App"):
+    st.write("The app is running!")
 
 st.title('ガラス工房へようこそ！')
 
