894affc0f4169b10b95cc0a378d4e5e9c7e99e78
RiLaFuS <ce26415@gmail.com>
2023-12-06 14:08:15 +0900

    10st commmit

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from sklearn.ensemble import RandomForestClassifier
+from typing import List
 import pickle
 
 # インスタンス化
 app = FastAPI()
 
 # 入力するデータ型の定義
-class glass(BaseModel):
+class Glass(BaseModel):
     RI: float
     Na: float
     Mg: float
@@ -21,12 +21,12 @@ class glass(BaseModel):
 # 学習済みのモデルの読み込み
 model = pickle.load(open('models/model_glass', 'rb'))
 
-# トップページ
-@app.get('/')
-def index():
-    return {"Glass": 'glass_prediction'}
-
-# POST が送信された時（入力）と予測値（出力）の定義
-@app.post('/predict')
-def make_predictions(features: glass):
-    return({'prediction':str(model.predict([[features.RI, features.Na, features.Mg, features.Al, features.Si, features.K, features.Ca, features.Ba, features.Fe]])[0])})
+# /predict エンドポイントの処理
+@app.post("/predict")
+async def predict(glass: Glass):
+    try:
+        # 予測の実行
+        prediction = model.predict([[glass.RI, glass.Na, glass.Mg, glass.Al, glass.Si, glass.K, glass.Ca, glass.Ba, glass.Fe]])[0]
+        return {"prediction": int(prediction)}  # 予測結果を整数型に変換して返す
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
