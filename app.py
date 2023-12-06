5b9b572e24fe37b3ea60a821db941c13ac4469a0
RiLaFuS <ce26415@gmail.com>
2023-12-06 15:25:09 +0900

    10st commmit

--- app.py
+++ app.py
@@ -50,6 +50,7 @@ if st.sidebar.button("できあがり"):
 
     # 予測の実行
     response = requests.post("https://glassapp-kh9owc32nt34xnlifhbkgd.streamlit.app/predict", json=df)
+    print(response.text)
     prediction = response.json()["prediction"]
 
     # 予測結果の表示
